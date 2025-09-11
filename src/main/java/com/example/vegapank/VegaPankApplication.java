4c025cdbd84a4f8827ef5d17421158a06705c428
Kirill <krupeto302@gmail.com>
2025-09-12 01:26:06 +0300

    Initial commit

---
+++ src/main/java/com/example/vegapank/VegaPankApplication.java
@@ -0,0 +1,13 @@
+package com.example.vegapank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VegaPankApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VegaPankApplication.class, args);
+	}
+
+}
